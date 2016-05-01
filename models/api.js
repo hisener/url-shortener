f4769f461355ec34f7c0ef86f53655940869f193
hisener <hisener@yahoo.com>
2016-05-01 21:19:37 +0300

    api route and model added

---
+++ models/api.js
@@ -0,0 +1,5 @@
+var db = require('./db.js');
+
+// todo: process url
+// todo: redirect short id
+// todo: or new 404
--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,12 @@
-var express = require('express'),
-    apiRouter = express.Router();
+var express = require('express');
+var apiRouter = express.Router();
+var api = require('../models/api');
 
-apiRouter.get('/:url', function(req, res, next) {
-  res.json({ });
+// todo: 80 characters limit
+var regex = /^\/([(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b[-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
+apiRouter.get(regex, function(req, res, next) {
+  res.json({ result: req.params["0"] });
+  // todo: send req.params["0"] to api model
 });
 
 module.exports = apiRouter;
